e5dba1b7ed96140a3e9f67761d8f7c2101925a96
pangtinghuan <pangtinghuan@tuandai.com>
2019-01-28 13:16:23 +0800

    将eureka server 从单机模式变成了集群模式

---
+++ spring-cloud-server-server-2/src/main/java/com/drafire/springcloudserverserver2/SpringCloudServerServer2Application.java
@@ -0,0 +1,16 @@
+package com.drafire.springcloudserverserver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudServerServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerServer2Application.class, args);
+	}
+
+}
+
