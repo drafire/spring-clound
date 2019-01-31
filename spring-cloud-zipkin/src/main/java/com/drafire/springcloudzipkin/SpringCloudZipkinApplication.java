dba6a831830d524ee325b4f7c80787e8e4cd2e87
pangtinghuan <pangtinghuan@tuandai.com>
2019-01-31 18:39:05 +0800

    新建sleuth、zipkin模块

---
+++ spring-cloud-zipkin/src/main/java/com/drafire/springcloudzipkin/SpringCloudZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.drafire.springcloudzipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZipkinApplication.class, args);
+	}
+
+}
+
